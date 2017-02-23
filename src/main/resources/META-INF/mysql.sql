c717eb70d1425f76101682ba5b7d854945e6e7ae
ZhongWen Li <imlaotse@gmail.com>
2017-02-23 15:19:20 +0800

    Update

--- src/main/resources/META-INF/mysql.sql
+++ src/main/resources/META-INF/mysql.sql
@@ -13,4 +13,9 @@ CREATE TABLE `employee` (
 
 INSERT INTO `employee` (`id`, `no`, `name`, `mail`, `password`, `phone`, `gender`)
 VALUES
-	(1,'0','admin','admin@gmail.com','000000','15618332942','F');
+	(1,'0001','admin','admin@gmail.com','000000','15618332942','F'),
+	(2,'0002','tiger','tiger@gmail.com','000000','15618332942','F'),
+	(3,'0003','fish','fish@gmail.com','000000','15618332942','F'),
+	(4,'0004','rabbit','rabbit@gmail.com','000000','15618332942','F'),
+	(5,'0005','docker','docker@gmail.com','000000','15618332942','F'),
+	;
